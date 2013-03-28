71bd4a848dc39164917921c00146d57f7a6d2f63
Tim Evans <tim.evans@paddle8.com>
2013-03-28 12:08:07 -0400

    add tests for rangeStartForIndex

--- tests/index-set/rangeStartForIndex.js
+++ tests/index-set/rangeStartForIndex.js
@@ -1,46 +1,40 @@
-// ==========================================================================
-// Project:   SproutCore - JavaScript Application Framework
-// Copyright: ©2006-2011 Apple Inc. and contributors.
-// License:   Licensed under MIT license (see license.js)
-// ==========================================================================
-
-/*global module test equals context ok same */
-var set, start, len ;
-module("SC.IndexSet#rangeStartForIndex", {
-  setup: function() {
-    start = SC.IndexSet.HINT_SIZE*2 + 10 ;
-    len  = Math.floor(SC.IndexSet.HINT_SIZE * 1.5);
-    set = SC.IndexSet.create().add(start, len);
+var set, start, len;
+module("IndexSet#rangeStartForIndex", {
+  setup: function () {
+    start = IndexSet.ENV.HINT_SIZE * 2 + 10;
+    len  = Math.floor(IndexSet.ENV.HINT_SIZE * 1.5);
+    set = new IndexSet();
+    set.addIndexesInRange(start, len);
   }
 });
 
-test("index is start of range", function() {
-  equals(set.rangeStartForIndex(start), start, 'should return start');
-  equals(set.rangeStartForIndex(0), 0, 'should return first range');
+test("index is start of range", function () {
+  equal(set.rangeStartForIndex(start), start);
+  equal(set.rangeStartForIndex(0), 0);
 });
 
-test("index is middle of range", function() {
-  equals(set.rangeStartForIndex(start+20), start, 'should return start');
-  equals(set.rangeStartForIndex(start+SC.IndexSet.HINT_SIZE), start, 'should return start');
-  equals(set.rangeStartForIndex(20), 0, 'should return first range');
+test("index is middle of range", function () {
+  equal(set.rangeStartForIndex(start + 20), start);
+  equal(set.rangeStartForIndex(start + IndexSet.ENV.HINT_SIZE), start);
+  equal(set.rangeStartForIndex(20), 0);
 });
 
-test("index last index", function() {
-  equals(set.rangeStartForIndex(start+len), start+len, 'should return end of range');
+test("index last index", function () {
+  equal(set.rangeStartForIndex(start + len), start + len);
 });
 
-test("index past last index", function() {
-  equals(set.rangeStartForIndex(start+len+20), start+len, 'should return end of range');
+test("index past last index", function () {
+  equal(set.rangeStartForIndex(start + len + 20), start + len);
 });
 
 test("creating holes by appending to an existing range should not affect the range start", function () {
-  var hintSize = SC.IndexSet.HINT_SIZE,
+  var hintSize = IndexSet.ENV.HINT_SIZE,
       start, set;
 
-  set = SC.IndexSet.create();
+  set = new IndexSet();
 
-  set.add(1);
-  set.add(hintSize + 1);
+  set.addIndex(1);
+  set.addIndex(hintSize + 1);
 
   // Before adding 2,
   // the internal data structure looks like:
@@ -52,12 +46,12 @@ test("creating holes by appending to an existing range should not affect the ran
   //   257:  258,   // End of range is 258
   //   258:    0    // End of index set
   // }
-  equals(set.rangeStartForIndex(hintSize),
-         set.rangeStartForIndex(hintSize - 1));
+  equal(set.rangeStartForIndex(hintSize),
+        set.rangeStartForIndex(hintSize - 1));
 
-  set.add(2);
+  set.addIndex(2);
 
-  // Assuming SC.IndexSet.HINT_SIZE is 256,
+  // Assuming IndexSet.ENV.HINT_SIZE is 256,
   // the internal data structure looks like:
   // {
   //   0  : -  1,   // Hole until 1
@@ -67,7 +61,6 @@ test("creating holes by appending to an existing range should not affect the ran
   //   257:  258,   // End of range is 258
   //   258:    0    // End of index set
   // }
-
-  equals(set.rangeStartForIndex(hintSize),
-         set.rangeStartForIndex(hintSize - 1));
+  equal(set.rangeStartForIndex(hintSize),
+        set.rangeStartForIndex(hintSize - 1));
 });
