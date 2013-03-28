77ddc46882d4555d224d3c40ced7db00fa9fd36d
Tim Evans <tim.evans@paddle8.com>
2013-03-28 12:32:12 -0400

    uncomment tests for the lastIndex

--- tests/index-set/last-index.js
+++ tests/index-set/last-index.js
@@ -16,25 +16,28 @@ test("after adding one range", function() {
   equal(set.lastIndex, 5);
 });
 
-/*
 test("after adding range then removing part of range", function() {
-  set.addIndexesInRange(4,4).remove(6,4);
-  equal(set.lastIndex,6, 'lastIndex should be one greater than lastIndex index');
+  set.addIndexesInRange(4, 4)
+     .removeIndexesInRange(6, 4);
+  equal(set.lastIndex, 5);
 });
-*/
+
 test("after adding range several disjoint ranges", function() {
   set.addIndexesInRange(4, 4)
      .addIndexesInRange(6000, 1);
   equal(set.lastIndex, 6000);
 });
-/*
+
 test("after removing disjoint range", function() {
-  set.addIndexesInRange(4,2).addIndexesInRange(6000).remove(5998,10);
-  equal(set.lastIndex,6, 'lastIndex should be one greater than lastIndex index');
+  set.addIndexesInRange(4, 2)
+     .addIndex(6000)
+     .removeIndexesInRange(5998, 10);
+  equal(set.lastIndex, 5);
 });
 
 test("after removing all ranges", function() {
-  set.addIndexesInRange(4,2).addIndexesInRange(6000).remove(3,6200);
-  equal(set.lastIndex, 0, 'lastIndex should be back to 0 with no content');
+  set.addIndexesInRange(4, 2)
+     .addIndex(6000)
+     .removeIndexesInRange(3, 6200);
+  equal(set.lastIndex, -1);
 });
-*/
