a4c87030593873d208d53cd4e9343d4dc7da1ed5
Tim Evans <tim.evans@paddle8.com>
2014-06-09 16:27:13 -0400

    bump Broccoli and QUnit

--- Brocfile.js
+++ Brocfile.js
@@ -1,47 +1,45 @@
-module.exports = function (broccoli) {
-  var uglifyJavaScript = require('broccoli-uglify-js'),
-      pickFiles = require('broccoli-static-compiler'),
-      compileES6 = require('broccoli-es6-concatenator'),
-      env = process.env.BROCCOLI_ENV || 'development';
+var uglifyJavaScript = require('broccoli-uglify-js'),
+    pickFiles = require('broccoli-static-compiler'),
+    mergeTrees = require('broccoli-merge-trees'),
+    compileES6 = require('broccoli-es6-concatenator'),
+    env = process.env.BROCCOLI_ENV || 'development';
 
-  var publicTree = broccoli.makeTree('public');
+var publicFiles = 'public';
 
-  var lib = broccoli.makeTree('lib');
-  lib = pickFiles(lib, {
-    srcDir: '/',
-    destDir: ''
-  });
+var lib = 'lib';
+lib = pickFiles(lib, {
+  srcDir: '/',
+  destDir: ''
+});
 
-  var vendor = broccoli.makeTree('vendor');
+var vendor = 'vendor';
+var libAndVendorTree = mergeTrees([lib, vendor]);
 
-  var libAndVendorTree = new broccoli.MergedTree([lib, vendor]);
+var libJs = compileES6(libAndVendorTree, {
+  loaderFile: 'loader.js',
+  inputFiles: [
+    '**/*.js'
+  ],
+  outputFile: '/index-set.js'
+});
 
-  var libJs = compileES6(libAndVendorTree, {
-    loaderFile: 'loader.js',
-    inputFiles: [
-      '**/*.js'
-    ],
-    outputFile: '/index-set.js'
-  });
+var tests = 'tests';
+tests = pickFiles(tests, {
+  srcDir: '/',
+  destDir: 'tests'
+});
 
-  var tests = broccoli.makeTree('tests');
-  tests = pickFiles(tests, {
-    srcDir: '/',
-    destDir: 'tests'
-  });
+var testsJs = mergeTrees([lib, vendor, tests]);
+testsJs = compileES6(testsJs, {
+  loaderFile: 'loader.js',
+  inputFiles: [
+    '**/*.js'
+  ],
+  outputFile: '/index-set.js'
+});
 
-  var testsJs = new broccoli.MergedTree([lib, vendor, tests]);
-  testsJs = compileES6(testsJs, {
-    loaderFile: 'loader.js',
-    inputFiles: [
-      '**/*.js'
-    ],
-    outputFile: '/index-set.js'
-  });
+if (env === 'test') {
+  libJs = testsJs;
+}
 
-  if (env === 'test') {
-    libJs = testsJs;
-  }
-
-  return [publicTree, libJs];
-};
+module.exports = mergeTrees([publicFiles, libJs]);
