9a3c1c398d9977096d5f89c671fb9546c1ec74bc
Tim Evans <tim.evans@paddle8.com>
2014-07-22 15:10:01 -0400

    update loader.js

--- dist/index-set.js
+++ dist/index-set.js
@@ -1,32 +1,128 @@
-var define, require;
+var define, requireModule, require, requirejs;
 
-(function () {
+(function() {
+
+  var _isArray;
+  if (!Array.isArray) {
+    _isArray = function (x) {
+      return Object.prototype.toString.call(x) === "[object Array]";
+    };
+  } else {
+    _isArray = Array.isArray;
+  }
+
   var registry = {}, seen = {};
+  var FAILED = false;
 
-  define = function (name, deps, callback) {
-    registry[name] = { deps: deps, callback: callback };
+  var uuid = 0;
+
+  function Module(name, deps, callback, exports) {
+    var defaultDeps = ['require', 'exports', 'module'];
+
+    this.id       = uuid++;
+    this.name     = name;
+    this.deps     = !deps.length && callback.length ? defaultDeps : deps;
+    this.exports  = exports || { };
+    this.callback = callback;
+    this.state    = undefined;
+  }
+
+  define = function(name, deps, callback) {
+    if (!_isArray(deps)) {
+      callback = deps;
+      deps     =  [];
+    }
+
+    registry[name] = new Module(name, deps, callback);
   };
 
-  require = function (name) {
-    if (seen[name]) { return seen[name]; }
-    seen[name] = {};
+  define.amd = {};
 
-    var mod = registry[name],
-        deps = mod.deps,
-        callback = mod.callback,
-        reified = [],
-        exports;
+  function reify(mod, name, seen) {
+    var deps = mod.deps;
+    var length = deps.length;
+    var reified = new Array(length);
+    var dep;
+    // TODO: new Module
+    // TODO: seen refactor
+    var module = { };
 
-    for (var i=0, l=deps.length; i<l; i++) {
-      if (deps[i] === 'exports') {
-        reified.push(exports = {});
+    for (var i = 0, l = length; i < l; i++) {
+      dep = deps[i];
+      if (dep === 'exports') {
+        module.exports = reified[i] = seen;
+      } else if (dep === 'require') {
+        reified[i] = require;
+      } else if (dep === 'module') {
+        mod.exports = seen;
+        module = reified[i] = mod;
       } else {
-        reified.push(require(deps[i]));
+        reified[i] = require(resolve(dep, name));
       }
     }
 
-    var value = callback.apply(this, reified);
-    return seen[name] = exports || value;
+    return {
+      deps: reified,
+      module: module
+    };
+  }
+
+  requirejs = require = requireModule = function(name) {
+    var mod = registry[name];
+    if (!mod) {
+      throw new Error('Could not find module ' + name);
+    }
+
+    if (mod.state !== FAILED &&
+        seen.hasOwnProperty(name)) {
+      return seen[name];
+    }
+
+    var reified;
+    var module;
+    var loaded = false;
+
+    seen[name] = { }; // placeholder for run-time cycles
+
+    try {
+      reified = reify(mod, name, seen[name]);
+      module = mod.callback.apply(this, reified.deps);
+      loaded = true;
+    } finally {
+      if (!loaded) {
+        mod.state = FAILED;
+      }
+    }
+
+    if (module === undefined && reified.module.exports) {
+      return (seen[name] = reified.module.exports);
+    } else {
+      return (seen[name] = module);
+    }
+  };
+
+  function resolve(child, name) {
+    if (child.charAt(0) !== '.') { return child; }
+
+    var parts = child.split('/');
+    var nameParts = name.split('/');
+    var parentBase = nameParts.slice(0, -1);
+
+    for (var i = 0, l = parts.length; i < l; i++) {
+      var part = parts[i];
+
+      if (part === '..') { parentBase.pop(); }
+      else if (part === '.') { continue; }
+      else { parentBase.push(part); }
+    }
+
+    return parentBase.join('/');
+  }
+
+  requirejs.entries = requirejs._eak_seen = registry;
+  requirejs.clear = function(){
+    requirejs.entries = requirejs._eak_seen = registry = {};
+    seen = state = {};
   };
 })();
 
@@ -1847,35 +1943,131 @@ var define, require;
   [],
   function() {
     "use strict";
-    var define, require;
+    var define, requireModule, require, requirejs;
 
-    (function () {
+    (function() {
+
+      var _isArray;
+      if (!Array.isArray) {
+        _isArray = function (x) {
+          return Object.prototype.toString.call(x) === "[object Array]";
+        };
+      } else {
+        _isArray = Array.isArray;
+      }
+
       var registry = {}, seen = {};
+      var FAILED = false;
 
-      define = function (name, deps, callback) {
-        registry[name] = { deps: deps, callback: callback };
+      var uuid = 0;
+
+      function Module(name, deps, callback, exports) {
+        var defaultDeps = ['require', 'exports', 'module'];
+
+        this.id       = uuid++;
+        this.name     = name;
+        this.deps     = !deps.length && callback.length ? defaultDeps : deps;
+        this.exports  = exports || { };
+        this.callback = callback;
+        this.state    = undefined;
+      }
+
+      define = function(name, deps, callback) {
+        if (!_isArray(deps)) {
+          callback = deps;
+          deps     =  [];
+        }
+
+        registry[name] = new Module(name, deps, callback);
       };
 
-      require = function (name) {
-        if (seen[name]) { return seen[name]; }
-        seen[name] = {};
+      define.amd = {};
 
-        var mod = registry[name],
-            deps = mod.deps,
-            callback = mod.callback,
-            reified = [],
-            exports;
+      function reify(mod, name, seen) {
+        var deps = mod.deps;
+        var length = deps.length;
+        var reified = new Array(length);
+        var dep;
+        // TODO: new Module
+        // TODO: seen refactor
+        var module = { };
 
-        for (var i=0, l=deps.length; i<l; i++) {
-          if (deps[i] === 'exports') {
-            reified.push(exports = {});
+        for (var i = 0, l = length; i < l; i++) {
+          dep = deps[i];
+          if (dep === 'exports') {
+            module.exports = reified[i] = seen;
+          } else if (dep === 'require') {
+            reified[i] = require;
+          } else if (dep === 'module') {
+            mod.exports = seen;
+            module = reified[i] = mod;
           } else {
-            reified.push(require(deps[i]));
+            reified[i] = require(resolve(dep, name));
           }
         }
 
-        var value = callback.apply(this, reified);
-        return seen[name] = exports || value;
+        return {
+          deps: reified,
+          module: module
+        };
+      }
+
+      requirejs = require = requireModule = function(name) {
+        var mod = registry[name];
+        if (!mod) {
+          throw new Error('Could not find module ' + name);
+        }
+
+        if (mod.state !== FAILED &&
+            seen.hasOwnProperty(name)) {
+          return seen[name];
+        }
+
+        var reified;
+        var module;
+        var loaded = false;
+
+        seen[name] = { }; // placeholder for run-time cycles
+
+        try {
+          reified = reify(mod, name, seen[name]);
+          module = mod.callback.apply(this, reified.deps);
+          loaded = true;
+        } finally {
+          if (!loaded) {
+            mod.state = FAILED;
+          }
+        }
+
+        if (module === undefined && reified.module.exports) {
+          return (seen[name] = reified.module.exports);
+        } else {
+          return (seen[name] = module);
+        }
+      };
+
+      function resolve(child, name) {
+        if (child.charAt(0) !== '.') { return child; }
+
+        var parts = child.split('/');
+        var nameParts = name.split('/');
+        var parentBase = nameParts.slice(0, -1);
+
+        for (var i = 0, l = parts.length; i < l; i++) {
+          var part = parts[i];
+
+          if (part === '..') { parentBase.pop(); }
+          else if (part === '.') { continue; }
+          else { parentBase.push(part); }
+        }
+
+        return parentBase.join('/');
+      }
+
+      requirejs.entries = requirejs._eak_seen = registry;
+      requirejs.clear = function(){
+        requirejs.entries = requirejs._eak_seen = registry = {};
+        seen = state = {};
       };
     })();
   });
